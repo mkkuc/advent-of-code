4f3f7f8a982668745f258fe1e874f2a0c948917e
Michał Kuc <michalkucit@gmail.com>
2023-12-01 13:20:01 +0100

    First task

---
+++ Advent/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Hello, World!");
+List<string> input = File.ReadAllLines("1.txt").ToList();
+
+int result = 0;
+
+foreach (string line in input)
+{
+    string firstSide = string.Empty;
+    string endSide = string.Empty;
+
+    for (int i = 0; i < line.Length; i++)
+    {
+        char character = line[i];
+        if (!int.TryParse(character.ToString(), out _))
+        {
+            continue;
+        }
+
+        firstSide = character.ToString();
+        break;
+    }
+
+    for (int i = line.Length - 1; i >= 0; i--)
+    {
+        char character = line[i];
+        if (!int.TryParse(character.ToString(), out _))
+        {
+            continue;
+        }
+
+        endSide = character.ToString();
+        break;
+    }
+
+    result += int.Parse((firstSide + endSide).ToString());
+}
+
+Console.WriteLine(result);
+Console.ReadKey();
