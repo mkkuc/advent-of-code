9a425b65f786eda45f1b957f6b990d126eea85b6
Michał Kuc <michalkucit@gmail.com>
2023-12-01 19:53:28 +0100

    2022 - Task2 part2

--- 2022/Task2/Program.cs
+++ 2022/Task2/Program.cs
@@ -3,17 +3,22 @@
 // ROCK         A X
 // PAPER        B Y
 // SCISSORS     C Z
+
+
+// LOSE     X
+// DRAW     Y
+// WIN      Z
 Dictionary<string, int> gameRules = new()
 {
-    { "A X", 4 },
-    { "A Y", 8 },
-    { "A Z", 3 },
+    { "A X", 3 },
+    { "A Y", 4 },
+    { "A Z", 8 },
     { "B X", 1 },
     { "B Y", 5 },
     { "B Z", 9 },
-    { "C X", 7 },
-    { "C Y", 2 },
-    { "C Z", 6 },
+    { "C X", 2 },
+    { "C Y", 6 },
+    { "C Z", 7 },
 };
 
 int result = 0;
