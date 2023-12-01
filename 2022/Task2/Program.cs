19422e4e9943a6603b8cc225cbf6f0fb33739502
Michał Kuc <michalkucit@gmail.com>
2023-12-01 19:46:17 +0100

    2022  - Task2 part1

---
+++ 2022/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿List<string> data = File.ReadAllLines("2.txt").ToList();
+
+// ROCK         A X
+// PAPER        B Y
+// SCISSORS     C Z
+Dictionary<string, int> gameRules = new()
+{
+    { "A X", 4 },
+    { "A Y", 8 },
+    { "A Z", 3 },
+    { "B X", 1 },
+    { "B Y", 5 },
+    { "B Z", 9 },
+    { "C X", 7 },
+    { "C Y", 2 },
+    { "C Z", 6 },
+};
+
+int result = 0;
+
+foreach (var item in data)
+{
+    result += gameRules.GetValueOrDefault(item);
+}
+
+Console.WriteLine(result);
