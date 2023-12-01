178df9296093eb996772a8d4510bcd94e77027a2
Michał Kuc <michalkucit@gmail.com>
2023-12-01 19:16:57 +0100

    2022 - task1 part 1

---
+++ 2022/Advent-2022/Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> data = File.ReadAllLines("1.txt").ToList();
+
+int maximum = 0;
+int tempMax = 0;
+string lastItem = string.Empty;
+
+foreach (string item in data)
+{
+    if (item.Equals(string.Empty))
+    {
+        maximum = maximum < tempMax ? tempMax : maximum;
+        tempMax = 0;
+        continue;
+    }
+
+    tempMax += int.Parse(item);
+    lastItem = item;
+}
+
+tempMax += int.Parse(lastItem);
+maximum = maximum < tempMax ? tempMax : maximum;
+
+Console.WriteLine(maximum);
+Console.Read();
