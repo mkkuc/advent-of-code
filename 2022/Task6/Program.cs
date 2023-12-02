e5d97c048903376dc43c7160b52507f7757c3317
Michał Kuc <michalkucit@gmail.com>
2023-12-02 19:19:35 +0100

    2022 - Task6 part1

---
+++ 2022/Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿List<string> data = File.ReadAllLines("6.txt").ToList();
+
+foreach (var line in data)
+{
+    string pack = string.Empty;
+
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (!pack.Contains(line[i]))
+        {
+            pack += line[i];
+            if (pack.Length == 4)
+            {
+                Console.WriteLine(i + 1);
+                break;
+            }
+            continue;
+        }
+        else
+        {
+            var indexOfLetterToRemove = pack.IndexOf(line[i]);
+            pack = pack.Substring(indexOfLetterToRemove + 1, pack.Length - (indexOfLetterToRemove + 1));
+            pack += line[i];
+        }
+    }
+}
