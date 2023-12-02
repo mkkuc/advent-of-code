76bfe1ba97194bc48070d5039be72d71d424cec9
Michał Kuc <michalkucit@gmail.com>
2023-12-02 17:20:42 +0100

    2022 - Task4 part1

---
+++ 2022/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿List<string> data = File.ReadAllLines("4.txt").ToList();
+
+int result = 0;
+
+foreach (string line in data)
+{
+    string[] ranges = line.Split(',');
+    string startRange = ranges[0];
+    string endRange = ranges[1];
+
+    int start1 = int.Parse(startRange.Split('-')[0]);
+    int start2 = int.Parse(startRange.Split('-')[1]);
+
+    int end1 = int.Parse(endRange.Split('-')[0]);
+    int end2 = int.Parse(endRange.Split('-')[1]);
+
+    if (start1 <= end1 && start2 >= end2)
+    {
+        result++;
+    }
+    else if (end1 <= start1 && end2 >= start2)
+    {
+        result++;
+    }
+}
+
+Console.WriteLine(result);
